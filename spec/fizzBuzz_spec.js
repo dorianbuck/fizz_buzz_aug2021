fc12ddeba490de4604fe2e860a2cf1b12bf798e3
Dorian Buck <[dorianbuck@gmail.com]>
2021-07-28 20:52:57 +0200

    implemented bdd-lazy-var text as well and it's now passing as well

--- spec/fizzBuzz_spec.js
+++ spec/fizzBuzz_spec.js
@@ -1,24 +1,17 @@
 const { expect } = require("chai");
 const FizzBuzz = require("../src/fizzBuzz.js");
-
-describe("fizzBuzz", () => {
-  let game;
-
-  before(() => {
-    game = new FizzBuzz();
-  });
-
+describe("FizzBuzz", () => {
+  subject(() => new FizzBuzz());
   it("is expected to return 1 if given 1", () => {
-    expect(game.play(1)).to.eql(1);
+    expect($subject.play(1)).to.eql(1);
   });
-
   it('is expected to return "fizz" if given 3', () => {
-    expect(game.play(3)).to.eql("fizz");
+    expect($subject.play(3)).to.eql("fizz");
   });
   it('is expected to return "buzz" if given 5', () => {
-    expect(game.play(5)).to.eql("buzz");
+    expect($subject.play(5)).to.eql("buzz");
   });
   it('is expected to return "fizz-buzz" if given 15', () => {
-    expect(game.play(15)).to.eql("fizz-buzz");
+    expect($subject.play(15)).to.eql("fizz-buzz");
   });
 });
